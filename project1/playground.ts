e2cb1bcca3d9d8b04c043b61477090f02ff188f7
Atul <dr6217811@gmail.com>
2026-04-23 20:38:29 +0530

    intersections and unions in typescript

--- project1/playground.ts
+++ project1/playground.ts
@@ -1,2 +1,14 @@
-type printName = (sum: number) => void
+interface Person {
+    name: String
+    age: number
+}
 
+interface newPerson extends Person  {email: string}
+
+const me: newPerson = {
+    name: 'Atul',
+    age: 23,
+    email: 'sdjks'
+}
+
+console.log(me)
