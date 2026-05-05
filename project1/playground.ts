54a3f5e972d1af5875be03709e37e790b710b0f5
Atul <dr6217811@gmail.com>
2026-05-05 16:23:45 +0530

    pick and omit in ts

--- project1/playground.ts
+++ project1/playground.ts
@@ -1,9 +1,21 @@
-function wait(duration: number): Promise<any> {
-    return new Promise<string>(resolve => {
-        setTimeout(() => resolve("Hi"), duration)
-    })
+type Todo = {
+    id: string
+    name: string
+    status: string
+    completed: boolean
 }
 
-wait(2000).then((value) => {
-    console.log(value.length) 
-})
+type NewTodo = Omit<Todo, "id">
+
+function saveTodo(todo: NewTodo) {
+    return {
+        ...todo,
+        id: crypto.randomUUID()
+    }
+}
+
+function renderTodo(todo: Todo) {
+    const div = document.createElement("div");
+    div.id = todo.id;
+
+}
